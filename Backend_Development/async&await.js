4b1c1e1d318db782c83e44376a4e71e612a1ff69
HemaDurgaKoppireddy <23P31A0530@acet.ac.in>
2025-12-04 11:12:21 +0530

    Add asynchronous functions using Promises and callbacks for user and student details

---
+++ Backend_Development/async&await.js
@@ -0,0 +1,15 @@
+function getUserDetails(id)
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{ 
+            resolve({userroll:"678"});
+        },2000);
+    })
+};
+const myfunc=async()=>{
+   console.log("123");
+   const result = await getUserDetails("123");
+   console.log(result);
+   console.log("456");
+};
+myfunc()
