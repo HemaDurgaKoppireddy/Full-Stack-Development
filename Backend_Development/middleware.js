d82b6332a8ce9317bdd671580e1cd6a5691e16d6
HemaDurgaKoppireddy <23P31A0530@acet.ac.in>
2025-12-06 11:44:53 +0530

    Implement Express server with CRUD endpoints for user management

---
+++ Backend_Development/middleware.js
@@ -0,0 +1,33 @@
+import express from 'express';
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+app.get('/get-user', (req, res) => {
+    res.send("api success");
+    // res.send('Hello, this is the get user endpoint!');
+})
+
+app.post('/add-user', (req, res) => {
+    let data = req.body;
+    console.log(data);
+    //logic to add data to database
+    res.send('data added successfully');
+})
+
+app.put('/edit-user', (req, res) => {
+    let mydata = req.body;
+    console.log(mydata);
+    res.send('data edited successfully');
+})
+
+app.delete('/delete-user', (req, res) => {
+    let mydata = req.body;
+    console.log(mydata);
+    res.send('data deleted successfully');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
